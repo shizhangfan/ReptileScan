c9b74172944da1abaac2cb38e15448814cfb1a76
shizhangfan <shizhangfan@ysb.cn>
2025-10-04 14:25:01 +0800

    Initial commit

---
+++ ReptileScan/ReptileScan/ReptileScanApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReptileScanApp.swift
+//  ReptileScan
+//
+//  Created by shizf on 2025/10/4.
+//
+
+import SwiftUI
+
+@main
+struct ReptileScanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
